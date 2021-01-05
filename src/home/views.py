042ef4a98a5640d1e9b2424af23fff5a9fe15f80
Mohameh reda <mohamed@ubuntu.ubuntu-domain>
2021-01-05 12:27:49 +0200

    Add More

--- src/home/views.py
+++ src/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from job.models import *
 # Create your views here.
 
 def home(request):
+    job_list       = Job.objects.all()
 
+
+    context = {
+        "jobs" : job_list,
+
+    }
     return render(request, 'home/index.html')
